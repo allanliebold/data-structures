39fd3d75dd6dc60fb2f4fdc1cf5cf4096eade93d
Allan Liebold <allan.liebold@gmail.com>
2017-10-23 15:37:17 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""."""
+from setuptools import setup
+
+setup(
+    name="data-structures",
+    description="Implementations of various data structures in Python",
+    version=0.1,
+    author="Matt Favoino, Allan Liebold",
+    licence="MIT",
+    py_modules=['linked-list'],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={
+        'testing': ['pytest', 'pytest-cov', 'tox'],
+        'development': ['ipython']
+    },
+    entry_points={}
+)
