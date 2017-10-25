a7c160b5e034c040e76a93ff3c627eab5dc48a95
Matt Favoino <mattfavoino@gmail.com>
2017-10-24 17:32:50 -0700

    Composition is happening. Push and pop seem to be working.

--- stack.py
+++ stack.py
@@ -1,1 +1,21 @@
 """File implements a stack with push and pop methods."""
+from linked_list import LinkedList
+from linked_list import Node
+
+
+class Stack(object):
+    """Stack class."""
+
+    def __init__(self, iterable=()):
+        """Init method for Stack."""
+        self._linkedlist = LinkedList(iterable)
+        print('stack created')
+
+    def push(self, value):
+        """Push method for Stack."""
+        self._linkedlist.push(value)
+        print('pushed')
+
+    def pop(self):
+        """Pop method for Stack."""
+        self._linkedlist.pop()
