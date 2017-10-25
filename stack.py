e9f281b67b7e5faea7170dab2deb7d272f6b1c3d
Matt Favoino <mattfavoino@gmail.com>
2017-10-24 17:36:29 -0700

    Reassigned names in init function for stack to easily access values

--- stack.py
+++ stack.py
@@ -1,6 +1,5 @@
 """File implements a stack with push and pop methods."""
 from linked_list import LinkedList
-from linked_list import Node
 
 
 class Stack(object):
@@ -9,6 +8,8 @@ class Stack(object):
     def __init__(self, iterable=()):
         """Init method for Stack."""
         self._linkedlist = LinkedList(iterable)
+        self.head = self._linkedlist.head
+        self._length = self._linkedlist._length
         print('stack created')
 
     def push(self, value):
