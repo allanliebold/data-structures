40a4d1b96f669869837fb0e11f574069fe98b531
Allan Liebold <allan.liebold@gmail.com>
2017-10-25 18:35:14 -0700

    adds two more tests to test_dll

--- test_dll.py
+++ test_dll.py
@@ -7,3 +7,35 @@ def test_dll_attr():
     from dll import Dll
     test_dll = Dll()
     assert test_dll._length == 0
+
+
+def test_dll_push_one():
+    """Test pushing to an empty list."""
+    from dll import Dll
+    test_dll = Dll()
+    test_dll.push(1)
+    assert test_dll.head.data == 1
+
+
+def test_dll_head_push_two():
+    """Test list with two nodes.
+
+    Head should point to next node.
+    """
+    from dll import Dll
+    test_dll = Dll()
+    test_dll.push(1)
+    test_dll.push(2)
+    assert test_dll.head.next.data == 1
+
+
+def test_dll_next_push_two():
+    """Test list with two nodes.
+
+    Second node should point back to head.
+    """
+    from dll import Dll
+    test_dll = Dll()
+    test_dll.push(1)
+    test_dll.push(2)
+    assert test_dll.head.next.prev.data == 2
