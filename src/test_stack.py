5938b79247a909fe15b14652e8315451a740f33e
Allan Liebold <allan.liebold@gmail.com>
2017-10-27 20:49:16 -0700

    writes more tests for stack.

--- src/test_stack.py
+++ src/test_stack.py
@@ -1,16 +1,40 @@
 """Tests for Stack Class."""
-import pytest
 
 
 def test_stack_init_empty():
-    """Tests initialization of Stack with no values passed."""
+    """Test initialization of Stack with no values passed."""
     from stack import Stack
     test_stack = Stack()
     assert test_stack._length == 0
 
 
-def test_stack_init_with_value():
+def test_stack_push_one_node():
+    """Test initializing stack with a value creates head node."""
+    from stack import Stack
+    test_stack = Stack()
+    test_stack.push(1)
+    assert test_stack._linkedlist.head.data == 1
+
+
+def test_stack_pop_one_node():
+    """Test that popping a stack of one node works correctly."""
+    from stack import Stack
+    test_stack = Stack()
+    test_stack.push(2)
+    test_stack.push(1)
+    test_stack.pop()
+    assert test_stack._linkedlist.head.data == 2
+
+
+def test_stack_init_with_iterable():
+    """Test that initializing stack with iterable has correct nodes."""
+    from stack import Stack
+    test_stack = Stack([1, 2, 3])
+    assert test_stack._linkedlist._length == 3
+
+
+def test_stack_pop_empty():
     """."""
     from stack import Stack
-    test_stack = Stack(1)
-    assert test_stack.head.value == 1
+    test_stack = Stack()
+    assert test_stack.pop() is None
