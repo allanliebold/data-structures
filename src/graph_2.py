2931d7f31dd515ac7c386e84bb53f36bb8454dd8
Allan Liebold <allan.liebold@gmail.com>
2017-11-07 15:36:22 -0800

    refactored dfs function to include visited list

--- src/graph_2.py
+++ src/graph_2.py
@@ -1,16 +1,29 @@
-"""Implement a graph traversal using composition from the original Graph class."""
+"""Implement graph traversal with composition from the original Graph class."""
 from graph import Graph
 
 
-
-def dfs(graph, start):
+def dfs(graph, node):
     """Depth-first traversal method for Graph class."""
     visited = []
-    visited.append(start)
-    start = graph.nodes[start]
-    for val in start:
-        print(val)
-        if val in visited:
-            return
-        visited.extend(dfs(graph, val))
-    return visited
+
+    def traverse(graph, node):
+        layer = []
+        visited.append(node)
+        layer.append(node)
+        node = graph.nodes[node]
+        for val in node:
+            if val not in visited:
+                layer.extend(traverse(graph, val))
+        return layer
+    return traverse(graph, node)
+
+
+if __name__ == '__main__':
+    g = Graph()
+    g.add_edge(1, 2)
+    g.add_edge(2, 3)
+    g.add_edge(2, 4)
+    g.add_edge(4, 1)
+    g.add_edge(4, 5)
+    print(g.nodes)
+    print(dfs(g, 1))
