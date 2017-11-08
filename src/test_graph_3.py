c8187b6f3cd2e2271dfb2e1684e9bb655cf6fcac
Allan Liebold <allan.liebold@gmail.com>
2017-11-08 13:28:30 -0800

    added weight to graph class and wrote a test.

---
+++ src/test_graph_3.py
@@ -0,0 +1,34 @@
+"""Test file for graph.py."""
+import pytest
+
+
+def test_graph_initialization():
+    """Test creation of graph object."""
+    from graph_3 import Graph
+    new_graph = Graph()
+    assert isinstance(new_graph.nodes, dict)
+
+
+def test_graph_add_node():
+    """Test adding a new node to empty graph."""
+    from graph_3 import Graph
+    new_graph = Graph()
+    new_graph.add_node(9)
+    assert 9 in new_graph.nodes
+
+
+def test_graph_add_dupe_node():
+    """Test adding a new node to empty graph."""
+    from graph_3 import Graph
+    new_graph = Graph()
+    new_graph.add_node(9)
+    with pytest.raises(ValueError):
+        new_graph.add_node(9)
+
+
+def test_graph_add_edge():
+    """Test adding an edge with a distance."""
+    from graph_3 import Graph
+    new_graph = Graph()
+    new_graph.add_edge('A', 'B', 6)
+    assert new_graph.nodes == {'A': {'B': 6}, 'B': {}}
