e46649916042896244c4b2f16ed9b9231560a8f3
Matt Favoino <mattfavoino@gmail.com>
2017-11-07 18:04:23 -0800

    bfs built without recursion.

--- src/graph_2.py
+++ src/graph_2.py
@@ -18,12 +18,24 @@ def dfs(graph, node):
     return traverse(graph, node)
 
 
+def bfs(graph, node):
+    """Breadth-first traversal method for Graph class."""
+    visited = []
+    route = [node]
+    while route:
+        current = route.pop(0)
+        if current not in visited:
+            visited.append(current)
+            route.extend(graph.nodes[current])
+    return visited
+
 if __name__ == '__main__':
     g = Graph()
     g.add_edge(1, 2)
-    g.add_edge(2, 3)
+    g.add_edge(1, 3)
     g.add_edge(2, 4)
-    g.add_edge(4, 1)
-    g.add_edge(4, 5)
+    g.add_edge(3, 5)
+    g.add_edge(3, 6)
     print(g.nodes)
     print(dfs(g, 1))
+    print(bfs(g, 1))
