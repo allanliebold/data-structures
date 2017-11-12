3f52fb76d1fd3ae145bfe2077ac2116a79f09f02
Allan Liebold <allan.liebold@gmail.com>
2017-11-11 16:09:40 -0800

    refactors shortest path. Return value is path in order.

--- src/shortest_path.py
+++ src/shortest_path.py
@@ -2,34 +2,38 @@
 from graph_3 import Graph
 
 
-def dijkstra(graph, start, end, visited=[], distances={}, routes={}):
-    """Function utilizing Dijkstra's algorithm.
+def shortest(graph, start, end):
+    shortest_path = []
 
-    Find the shortest path from a starting node to and end node.
-    """
-    if start == end:
-        shortest_path = []
-        path_str = ''
-        pred = end
-        while pred is not None:
-            shortest_path.append(pred)
-            path_str += pred
-            pred = routes.get(pred, None)
-        print(shortest_path[::-1])
-        return shortest_path[::-1]
-    else:
-        if not visited:
-            distances[start] = 0
-        for edge in graph.nodes[start]:
-            if edge not in visited:
-                new_distance = distances[start] + graph.nodes[start][edge]
-                if new_distance < distances.get(edge, float('inf')):
-                    distances[edge] = new_distance
-                    routes[edge] = start
-        visited.append(start)
-        unvisited = {}
-        for node in graph.nodes:
-            if node not in visited:
-                unvisited[node] = distances.get(node, float('inf'))
-        new_start = min(unvisited, key=unvisited.get)
-        dijkstra(graph, new_start, end, visited, distances, routes)
+    def dijkstra(graph, start, end, visited=[], distances={}, routes={}):
+        """Function utilizing Dijkstra's algorithm.
+
+        Find the shortest path from a starting node to and end node.
+        """
+        if start == end:
+            path_str = ''
+            pred = end
+            while pred is not None:
+                shortest_path.append(pred)
+                path_str += pred
+                pred = routes.get(pred, None)
+            return shortest_path[::-1]
+        else:
+            if not visited:
+                distances[start] = 0
+            for edge in graph.nodes[start]:
+                if edge not in visited:
+                    new_distance = distances[start] + graph.nodes[start][edge]
+                    if new_distance < distances.get(edge, float('inf')):
+                        distances[edge] = new_distance
+                        routes[edge] = start
+            visited.append(start)
+            unvisited = {}
+            for node in graph.nodes:
+                if node not in visited:
+                    unvisited[node] = distances.get(node, float('inf'))
+            new_start = min(unvisited, key=unvisited.get)
+            dijkstra(graph, new_start, end, visited, distances, routes)
+
+    dijkstra(graph, start, end)
+    return shortest_path[::-1]
--- src/test_shortest_path.py
+++ src/test_shortest_path.py
@@ -4,5 +4,5 @@ import pytest
 
 def test_dijkstra_returns_correct_order(weighted_graph):
     """Test dijkstra returns correct path."""
-    from shortest_path import dijkstra
-    assert dijkstra(weighted_graph, 'a', 'f') == ['a', 'c', 'e', 'f']
+    from shortest_path import shortest
+    assert shortest(weighted_graph, 'a', 'f') == ['a', 'c', 'e', 'f']
