d6756f79362c701d5921bef74619bddb99fed5fa
Allan Liebold <allan.liebold@gmail.com>
2017-11-20 20:29:43 -0800

    adds search and size methods and writes tests

--- src/bst.py
+++ src/bst.py
@@ -17,33 +17,52 @@ class BST(object):
     def __init__(self, iterable=()):
         """Initialization of BST class object."""
         self.root = None
-        self.size = 0
+        self._size = 0
 
         if isinstance(iterable, (tuple, list)):
             for i in iterable:
                 self.insert(i)
         else:
-            self.push(iterable)
+            self.insert(iterable)
 
     def insert(self, data):
         """Add node to tree."""
         new_node = Node(data)
         if not self.root:
             self.root = new_node
-            self.size += 1
+            self._size += 1
             return
 
         curr = self.root
         while curr:
+            if new_node.data == curr.data:
+                return "Duplicate data"
+
             if new_node.data < curr.data:
                 if not curr.left:
                     curr.left = new_node
-                    self.size += 1
+                    self._size += 1
                     return
                 curr = curr.left
             if new_node.data > curr.data:
                 if not curr.right:
                     curr.right = new_node
-                    self.size += 1
+                    self._size += 1
                     return
                 curr = curr.right
+
+    def search(self, data):
+        """Return node with specified data, or None."""
+        curr = self.root
+        while curr:
+            if curr.data == data:
+                return curr
+            elif data < curr.data:
+                curr = curr.left
+            elif data > curr.data:
+                curr = curr.right
+        return None
+
+    def size(self):
+        """Return number of nodes in tree."""
+        return self._size
--- src/test_bst.py
+++ src/test_bst.py
@@ -21,6 +21,11 @@ def test_bst_three_root(bst_three):
     assert bst_three.root.right.data == 15
 
 
+def test_insert_duplicate_data(bst_three):
+    """Test return when trying to insert data already in tree."""
+    assert bst_three.insert(5) == "Duplicate data"
+
+
 def test_insert_node_in_bst_three(bst_three):
     """Test node is inserted in the correct place."""
     bst_three.insert(13)
@@ -29,4 +34,30 @@ def test_insert_node_in_bst_three(bst_three):
 
 def test_size_bst_three(bst_three):
     """Test size is corrent on bst with three nodes."""
-    assert bst_three.size == 3
+    assert bst_three.size() == 3
+
+
+def test_size_bst_three_insert_one(bst_three):
+    """Test size is corrent on bst with three nodes after insert."""
+    bst_three.insert(8)
+    assert bst_three.size() == 4
+
+
+def test_search_valid_data(bst_three):
+    """Test search for data in tree returns correct node data."""
+    assert bst_three.search(15).data == 15
+
+
+def test_search_valid_data_return_node(bst_three):
+    """Test search for data in tree returns correct node."""
+    assert bst_three.search(5) == bst_three.root.left
+
+
+def test_search_invalid_data(bst_three):
+    """Test search for data not in tree returns None."""
+    assert bst_three.search(22) is None
+
+
+def test_search_data_in_root(bst_three):
+    """Test search for data in root returns root node."""
+    assert bst_three.search(10) == bst_three.root
