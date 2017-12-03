9c476ed23be3aedfd707c7c833f0e41a92b4b473
Allan Liebold <allan.liebold@gmail.com>
2017-12-03 12:58:42 -0800

    adds one at a time hash function.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     licence="MIT",
     py_modules=['linked-list', 'stack', 'dll', 'que_', 'deque',
                 'priorityq', 'binheap', 'graph', 'graph_2', 'graph_3',
-                'bst'],
+                'bst', 'hash'],
     package_dir={'': 'src'},
     install_requires=[],
     extras_require={
